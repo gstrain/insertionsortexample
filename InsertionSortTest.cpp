1bff9a21297b2aacb4b034b1b4d5c4436da0d4b8
Abe Ramseyer <abe.ramseyer@gmail.com>
2017-03-09 21:19:28 -0600

    hehe exponential -> quadratic time

--- InsertionSortTest.cpp
+++ InsertionSortTest.cpp
@@ -5,17 +5,14 @@
 int main() {
   std::random_device rd; // device for seeding generator
   std::mt19937 rng(rd()); // seed random gen with device
-  std::uniform_int_distribution<int> dist(0, 100); // create distribution
+  std::uniform_int_distribution<int> dist(0, 100000); // create distribution
 
   LinkedList myList;
 
-  for(int i=100000; i > 0; i--) {
+  for(int i=0; i < 100000; i++)
     myList.add(dist(rng));      // populate list with random values
-  }
-
-  // myList.printForward();  // print unsorted
+  
+  myList.printForward();  // print unsorted
   myList.sort();          // calls embedded insertion sort
-
   myList.printForward();  // print sorted
-  // myList.printBackward();
 }
